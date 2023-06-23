ceb286b96451bc02d742ae18dd705777ab04961d
rayhan ramadhan <117635384+rrayhanramadhan@users.noreply.github.com>
2023-06-24 03:20:11 +0700

    Add files via upload

---
+++ Pengumpulan Poin Versi 2/Form1.cs
@@ -0,0 +1,75 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace Pengumpulan_Poin_Versi_2
+{
+    public partial class Form1 : Form
+    {
+        private object userId;
+        private object pointsToAdd;
+        private int number1 = 0;
+        private int number2 = 0;
+        internal object button12;
+        internal object button13;
+        internal readonly object button14;
+
+        
+            
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void pictureBox10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label3_Click(object sender, EventArgs e)
+        {
+            Form5 form5 = new Form5();
+            form5.Show();
+            this.Hide();
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void guna2Panel4_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+    
+}
